16ac7d07792e0b1a22b09fded27c25487e76651a
Bukabuka321 <20uva02@gmail.com>
2023-03-17 12:41:08 +0100

    overview part desktop version

--- frontend/components/Overview/OverviewCard.tsx
+++ frontend/components/Overview/OverviewCard.tsx
@@ -10,15 +10,15 @@ interface Props {
 const OverviewCArd: FC<Props> = ({ logo, description, name }) => {
   return (
     <>
-      <div className="flex flex-col bg-[#22221f] rounded-2xl py-7 px-9  gap-y-6">
-        <div className="flex items-center gap-x-5">
-          <div className="py-4 px-4 bg-[#fac32029] rounded-xl">
+      <div className="flex flex-col bg-[#22221f] rounded-2xl py-7 px-9  gap-y-6 lg:basis-[43%] lg:px-6 lg:py-6">
+        <div className="flex flex-row items-center gap-x-4 lg:flex-col lg:items-start lg:gap-y-4">
+          <div className="py-3 px-3 bg-[#fac32029] rounded-lg lg:py-6 lg:px-6">
             <Image src={logo} alt="calendar" width={40} height={40} />
           </div>
-          <h2 className="font-bold text-3xl">{name}</h2>
+          <h2 className="font-bold text-3xl lg:text-2xl">{name}</h2>
         </div>
         <div>
-          <p className="font-light text-lg">{description}</p>
+          <p className="font-light text-lg lg:text-base">{description}</p>
         </div>
       </div>
     </>

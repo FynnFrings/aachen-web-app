5b09fbab2f24653edbeb74b14572052f673a33f0
Bukabuka321 <20uva02@gmail.com>
2023-04-04 16:46:07 +0200

    created 404 page, changed impressum, changed text in ad download

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <div className="text-white w-screen flex flex-col items-center justify-center h-screen gap-y-6 bg-[url('../public/gradient.png')] bg-cover bg-top -mx-[6%]">
+        <h1 className="font-bold text-5xl">404</h1>
+        <p>Kaiser Karl ist veschwunden</p>
+        <Link href="/">
+          <p className="rounded-lg bg-[#FAC520] px-4 py-4 text-black">
+            Zurück zur Startseite
+          </p>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;

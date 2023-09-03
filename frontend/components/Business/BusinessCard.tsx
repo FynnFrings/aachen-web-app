76b1754cd78ba595d2bee22c378852918c881f4d
Nidilko Viacheslav <19uvs99@gmail.com>
2023-09-03 22:55:51 +0300

    adding business [id] page

--- frontend/components/Business/BusinessCard.tsx
+++ frontend/components/Business/BusinessCard.tsx
@@ -8,66 +8,71 @@ import { MouseEventHandler } from "react";
 
 // Return JSX to render the business card
 const BusinessCard = ({
-  handleSubmit,
-  company,
+	handleSubmit,
+	company,
 }: {
-  handleSubmit: MouseEventHandler;
-  company: any;
+	handleSubmit: MouseEventHandler;
+	company: any;
 }) => {
-  return (
-    //Wrap an entire card in a link, which will redirect to business details page
-    <Link href={`/business/${company.company.name}`}>
-      <div className={styles.container}>
-        {/* Render a placeholder image */}
-        <div className={styles.business_image_container}>
-          <Image
-            className={styles.business_image}
-            src={MockupImage}
-            alt="business_image"
-            width={0}
-            height={0}
-          />
-          {/* Render a placeholder logo */}
-          <div className={styles.business_logo_container}>
-            <Image
-              className={styles.business_logo}
-              src={MockupImage2}
-              alt="business_image"
-              width={0}
-              height={0}
-            />
-          </div>
-        </div>
-        {/* Render the name, category, opening hours, button */}
-        <div className={styles.info}>
-          <div className={styles.business_name}>{company.company.name}</div>
-          <div className={styles.business_category}>Einzelhandel</div>
-          <p className={styles.dot}>•</p>
-          <div className={styles.business_time}>Geöffnet bis 23:00 Uhr</div>
-        </div>
-        {/* Render the additional information about distance and number of posts */}
-        <div className={styles.add_info}>
-          <div className={styles.distance}>
-            <p className={styles.number}>521m</p>
-            <p className={styles.text}>entfernt</p>
-          </div>
-          <div className={styles.posts}>
-            <p className={styles.number}>0</p>
-            <p className={styles.text}>Beiträge</p>
-          </div>
-          <button className={styles.business_button} onClick={handleSubmit}>
-            Merken
-            <Image
-              src={Notification}
-              alt={"notification"}
-              width={20}
-              height={20}
-            />
-          </button>
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		//Wrap an entire card in a link, which will redirect to business details page
+		<Link href={`/business/${company.itemId}`}>
+			<div className={styles.container}>
+				{/* Render a placeholder image */}
+				<div className={styles.business_image_container}>
+					<Image
+						className={styles.business_image}
+						src={MockupImage}
+						alt="business_image"
+						width={0}
+						height={0}
+					/>
+					{/* Render a placeholder logo */}
+					<div className={styles.business_logo_container}>
+						<Image
+							className={styles.business_logo}
+							src={MockupImage2}
+							alt="business_image"
+							width={0}
+							height={0}
+						/>
+					</div>
+				</div>
+				{/* Render the name, category, opening hours, button */}
+				<div className={styles.info}>
+					<div className={styles.business_name}>{company.name}</div>
+					<div className={styles.business_category}>Einzelhandel</div>
+					<p className={styles.dot}>•</p>
+					<div className={styles.business_time}>
+						Geöffnet bis 23:00 Uhr
+					</div>
+				</div>
+				{/* Render the additional information about distance and number of posts */}
+				<div className={styles.add_info}>
+					<div className={styles.distance}>
+						<p className={styles.number}>521m</p>
+						<p className={styles.text}>entfernt</p>
+					</div>
+					<div className={styles.posts}>
+						<p className={styles.number}>0</p>
+						<p className={styles.text}>Beiträge</p>
+					</div>
+					<button
+						className={styles.business_button}
+						onClick={handleSubmit}
+					>
+						Merken
+						<Image
+							src={Notification}
+							alt={"notification"}
+							width={20}
+							height={20}
+						/>
+					</button>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default BusinessCard;
--- frontend/pages/business/[id].tsx
+++ frontend/pages/business/[id].tsx
@@ -3,161 +3,202 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/business_details.module.scss";
 import EventMap from "../../components/map";
+import MockupImage from "@/public/AachenPics/aachen6.png";
+import MockupImage2 from "@/public/aachen_pic_2.png";
 import { MouseEventHandler, useState } from "react";
 import BusinessMerkenResponseMessage from "@/components/Business/BusinessMerkenResponseMessage";
 
-const BusinessDetailsPage = ({ companies  }: any) => {
-  const [alert, isAlert] = useState<boolean>(false);
-
-  // console.log(companies);
-
-  // onClick function with setTimeout fuction to manage "alert" state
-  const handleSubmit: MouseEventHandler = () => {
-    isAlert(true);
-    setTimeout(() => {
-      isAlert(false);
-    }, 3000);
-  };
-
-  const center: [number, number] = [51.505, -0.09];
-  return (
-    <>
-      <div className={styles.business_header}>
-        <div className={styles.background}>
-          <Image
-            src={"/business_logo.png"}
-            width="0"
-            height="0"
-            sizes="(max-width: 200px)"
-            alt="Business logo"
-            className={styles.logo}
-          />
-        </div>
-        <div className={styles.title}>
-          <h1>Weltweit am Dom</h1>
-          <p>
-            Einzelhandel{" "}
-            <span className={styles.opening_time}>Öffnet 9 Uhr</span>
-          </p>
-          <p className={styles.description}>
-            Fairtrade Artikel, Geschenkideen und Kindsthandwerk aus aller Welt.
-            Ehrenamtlich geführt!
-          </p>
-        </div>
-        <div className={styles.buttons}>
-          <button onClick={handleSubmit} className={styles.notification}>
-            <Image
-              alt="notification icon"
-              src={"/notification_icon.svg"}
-              height={"24"}
-              width={"24"}
-            />
-            Merken
-          </button>
-          {alert ? <BusinessMerkenResponseMessage /> : ""}
-          <Link href={"/kontaktiren"}>
-            <button className={styles.contact}>Kontaktieren</button>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.business_details}>
-        <div className={styles.information}>
-          <h2>Information</h2>
-          <p>
-            <Image alt="" height={"28"} width={"28"} src={"/building.svg"} />{" "}
-            Weltweit am Dom
-          </p>
-          <p>
-            <Image alt="" height={"28"} width={"28"} src={"/graph.svg"} />{" "}
-            Einzelhandel
-          </p>
-          <p>
-            <Image alt="" height={"28"} width={"28"} src={"/shop-remove.svg"} />{" "}
-            seit 24 Monaten Mitglied der Aachen App
-          </p>
-          <p>
-            <Image alt="" height={"28"} width={"28"} src={"/ticket-star.svg"} />{" "}
-            Noch keine Coupons erstellt
-          </p>
-          <p>
-            <Image alt="" height={"28"} width={"28"} src={"/calendar.svg"} />{" "}
-            Noch keine Events erstellt
-          </p>
-        </div>
-        <div className={styles.opening_time_table}>
-          <h2>Öffnungszeiten</h2>
-          <p>
-            Montag:<span>Unbekannt</span>
-          </p>
-          <p>
-            Dienstag:<span>Unbekannt</span>
-          </p>
-          <p>
-            Mittwoch:<span>Unbekannt</span>
-          </p>
-          <p>
-            Donnerstag:<span>Unbekannt</span>
-          </p>
-          <p>
-            Freitag:<span>Unbekannt</span>
-          </p>
-          <p>
-            Samstag:<span>Unbekannt</span>
-          </p>
-          <p>
-            Sonntag:<span>Unbekannt</span>
-          </p>
-        </div>
-        <div className={styles.location}>
-          <h2>Standort</h2>
-          {/* <iframe
-            className={styles.map}
-            loading="lazy"
-            allowFullScreen={true}
-            referrerPolicy="no-referrer-when-downgrade"
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAVaJhLqgWQgtXC4jjjHCHmspVsLIJHFaE
-    &q=Weltweit+am+Dom,Aachen+Germany&zoom=18"
-          /> */}
-          <EventMap
-            latitude={50.774688851658695}
-            longitude={6.083457278391105}
-          />
-          <Link
-            href={`https://maps.google.com/?q=${50.774688851658695},${6.083457278391105}`}
-          >
-            <button>Route planen</button>
-          </Link>
-        </div>
-        <div className={styles.website_link}>
-          <Image
-            src={"/business_logo.png"}
-            width="56"
-            height="56"
-            alt="Business logo"
-            className={styles.logo}
-          />
-          <span>
-            <h2>Website</h2>
-            <a
-              target="_blank"
-              href="https://www.bistum-aachen.de/Weltkirche-im-Bistum-Aachen/Weltweit-am-Dom/"
-              rel="noreferrer"
-            >
-              https://www.bistum-aachen.de/Weltkirche-im-Bistum-Aachen/Weltweit-am-Dom/
-            </a>
-          </span>
-        </div>
-      </div>
-    </>
-  );
+const BusinessDetailsPage = ({ company }: any) => {
+	const [alert, isAlert] = useState<boolean>(false);
+	// onClick function with setTimeout fuction to manage "alert" state
+	const handleSubmit: MouseEventHandler = () => {
+		isAlert(true);
+		setTimeout(() => {
+			isAlert(false);
+		}, 3000);
+	};
+	return (
+		<>
+			<div className={styles.business_header}>
+				<div className={styles.image_container}>
+					<Image
+						className={styles._image}
+						src={MockupImage}
+						alt="business_image"
+						width={0}
+						height={0}
+					/>
+					{/* Render a placeholder logo */}
+					<Image
+						className={styles._logo}
+						src={MockupImage2}
+						alt="business_logo"
+						width={0}
+						height={0}
+					/>
+				</div>
+				<div className={styles.title}>
+					<h1>{company.name}</h1>
+					<p>
+						Einzelhandel{" "}
+						<span className={styles.opening_time}>
+							Öffnet 9 Uhr
+						</span>
+					</p>
+					<p className={styles.description}>
+						Fairtrade Artikel, Geschenkideen und Kindsthandwerk aus
+						aller Welt. Ehrenamtlich geführt!
+					</p>
+				</div>
+				<div className={styles.buttons}>
+					<button
+						onClick={handleSubmit}
+						className={styles.notification}
+					>
+						<Image
+							alt="notification icon"
+							src={"/notification_icon.svg"}
+							height={"24"}
+							width={"24"}
+						/>
+						Merken
+					</button>
+					{alert ? <BusinessMerkenResponseMessage /> : ""}
+					<Link href={"/kontaktiren"}>
+						<button className={styles.contact}>Kontaktieren</button>
+					</Link>
+				</div>
+			</div>
+			<div className={styles.business_details}>
+				<div className={styles.information}>
+					<h2>Information</h2>
+					<p>
+						<Image
+							alt=""
+							height={"28"}
+							width={"28"}
+							src={"/building.svg"}
+						/>{" "}
+						{company.name}
+					</p>
+					<p>
+						<Image
+							alt=""
+							height={"28"}
+							width={"28"}
+							src={"/graph.svg"}
+						/>{" "}
+						{company.category}
+					</p>
+					<p>
+						<Image
+							alt=""
+							height={"28"}
+							width={"28"}
+							src={"/shop-remove.svg"}
+						/>{" "}
+						seit 24 Monaten Mitglied der Aachen App
+					</p>
+					<p>
+						<Image
+							alt=""
+							height={"28"}
+							width={"28"}
+							src={"/ticket-star.svg"}
+						/>{" "}
+						Noch keine Coupons erstellt
+					</p>
+					<p>
+						<Image
+							alt=""
+							height={"28"}
+							width={"28"}
+							src={"/calendar.svg"}
+						/>{" "}
+						Noch keine Events erstellt
+					</p>
+				</div>
+				<div className={styles.opening_time_table}>
+					<h2>Öffnungszeiten</h2>
+					<p>
+						Montag:<span>Unbekannt</span>
+					</p>
+					<p>
+						Dienstag:<span>Unbekannt</span>
+					</p>
+					<p>
+						Mittwoch:<span>Unbekannt</span>
+					</p>
+					<p>
+						Donnerstag:<span>Unbekannt</span>
+					</p>
+					<p>
+						Freitag:<span>Unbekannt</span>
+					</p>
+					<p>
+						Samstag:<span>Unbekannt</span>
+					</p>
+					<p>
+						Sonntag:<span>Unbekannt</span>
+					</p>
+				</div>
+				<div className={styles.location}>
+					<h2>Standort</h2>
+					<EventMap latitude={1} longitude={1} />
+					<div className={styles.route}>
+						<Link
+							target="_blank"
+							rel="noreferrer"
+							href={`https://maps.google.com/?q=${1},${1}`}
+						>
+							Route planen
+						</Link>
+						<span>
+							&quot;{"company.name"}&quot;, {"address.street"},{" "}
+							{"address.city"}
+						</span>
+					</div>
+				</div>
+				<div className={styles.website_link}>
+					<Image
+						src={"/business_logo.png"}
+						width="56"
+						height="56"
+						alt="Business logo"
+						className={styles.logo}
+					/>
+					<span>
+						<h2>Website</h2>
+						<a target="_blank" href={`http:/*`} rel="noreferrer">
+							{/* {website} */}
+						</a>
+					</span>
+				</div>
+			</div>
+		</>
+	);
 };
 
-export async function getServerSideProps() {
-  // Fetch data from  API
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const data = await res.json();
-  // Pass data to the page via props
-  return { props: { companies: data } };
+export async function getServerSideProps(context: { params: { id: string } }) {
+	// Fetch data from  API
+	const id = context.params.id;
+
+	// Declared url of events id
+	const businessUrlId: string =
+		"https://us-central1-aachen-app.cloudfunctions.net/getBusinessById"; //`http://localhost:5050/blog/${id}`;
+
+	// Fetching data
+	const res = await fetch(`${businessUrlId}`, {
+		method: "POST",
+		mode: "cors",
+		body: JSON.stringify({ id: id }),
+	});
+
+	// Store in "data" as json file
+	const data = await res.json();
+
+	// Pass data to the page via props
+	return { props: { company: data } };
 }
 
 export default BusinessDetailsPage;

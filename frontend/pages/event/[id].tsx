3557e8624bf78b7787ff81cf27fb8f80eb4df1f6
Viacheslav Nidilko <nidilko.viacheslav@gmail.com>
2023-09-11 01:34:34 +0300

    initial event and event_details page

---
+++ frontend/pages/event/[id].tsx
@@ -0,0 +1,8 @@
+const EventDetails = () => {
+	return (
+		<>
+			<h1>Events Details</h1>
+		</>
+	);
+};
+export default EventDetails;
---
+++ frontend/pages/event/index.tsx
@@ -0,0 +1,11 @@
+const Events = () => {
+	return (
+		<>
+			<div>
+				<h1>Events Page</h1>
+			</div>
+		</>
+	);
+};
+
+export default Events;

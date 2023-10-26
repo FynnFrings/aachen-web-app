73bbe3c2d446723cba631332bc3ef6a1ef87f863
Bukabuka321 <20uva02@gmail.com>
2023-10-26 16:21:34 +0200

    small fast fix

--- frontend/components/Nothing.tsx
+++ frontend/components/Nothing.tsx
@@ -1,7 +1,9 @@
 const Nothing = ({ list_name }: { list_name: string }) => {
 	return (
 		<>
-			<div className="text-white text-2xl">Leider es wurde keine {list_name} gefunden</div>
+			<div className="text-white text-lg lg:text-2xl text-center">
+				Leider es wurde keine {list_name} gefunden
+			</div>
 		</>
 	);
 };
